419807eec03a38419ec30dd17c90b2347338cfdd
ashutoshdil <rahul.a@dil.in>
2025-01-01 18:25:28 +0530

    ui update

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,22 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Badge, Box, Grid, Typography } from '@mui/material'
 import React, { useEffect, useRef, useState } from 'react'
 import HomeNavbar from 'src/sections/top-nav'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(planName, month, createdAt, updatedAt, status) {
+  return { planName, month, createdAt, updatedAt, status };
+}
+const rows = [
+  createData('Swami Sales Trial',1, '12 Dec 2024', '12 Jan 2025', "Expried"),
+];
+
 
 function dashboard() {
 
@@ -36,56 +52,125 @@ function dashboard() {
 
 
 <Grid container sx={{
-  marginTop : (appBarHeight+10)+"px",
+    marginTop : (appBarHeight+10)+"px",
+    display : 'flex',
+    justifyContent : 'center'
   }}>
-  <Grid xs={12} md={3}  sx={{
-    boxShadow : 3,
-    p : 10
-  }}>
-    <Box sx={{
-      display : 'flex',
-      flexDirection : 'column'
-    }}>
-      <Typography  sx={{
-        textAlign : 'center',
-        fontWeight : 'light',
-        fontFamily : 'inherit',
-        my : 1
-        }} variant='h4'>
-        Active Plan
-      </Typography>
+    <Grid md={10} xs={12}>
+      <Grid xs={12} md={1.8}  sx={{
+        boxShadow : 3,
+        borderRadius : 2,
+        m : 10
+      }}>
+        <Box sx={{px: 5}}>
+          {/* <Badge badgeContent={"Active"} color="success" /> */}
+            <Badge badgeContent={"Expired"} color="error" />
+        </Box>
+
+      <Box sx={{
+        p : 2
+      }}>
+        <Box sx={{
+          display : 'flex',
+          flexDirection : 'column',
+          justifyContent : 'center',
+          alignItems : 'center',
+        }}>
+          <Typography  sx={{
+            fontWeight : 'light',
+            fontFamily : 'inherit',
+            fontSize : 22
+            }} variant='h4'>
+            Active Plan
+          </Typography>
 
 
-      <Typography  sx={{
-        textAlign : 'center',
-        my : 2
-        }} variant='h4'>
-        100
-      </Typography>
+          <Typography  sx={{
+            my : 2
+            }} variant='h4'>
+            ₹ 100
+            <br/>
+            <span style={{
+              color : '#6c757d!important',
+              fontSize :14
+            }}>
+              6 month's plan
+            </span>
+          </Typography>
 
-    </Box>
+        </Box>    
 
-    <Box sx={{
-      display : 'flex'
-    }}>
-      <Typography  sx={{
-        textAlign : 'center',
-        fontWeight : 'light',
-        mx : 5
-        }} variant='small'>
-        Created Date : 05 Dec 2024
-      </Typography>
+        <Box sx={{
+          display : 'flex',
+          justifyContent : 'center',
+        }}>
+          <Typography  sx={{
+            fontWeight : 'light',
+            mx : 5
+            }} variant='small'>
+            Created Date
+            <br/>
+            <span style={{
+              color : '#6c757d!important',
+              fontSize : 14
+            }}>
+              05 Dec 2024
+            </span>
+          </Typography>
 
-      <Typography  sx={{
-        textAlign : 'center',
-        fontWeight : 'light'
-        }} variant='small'>
-        Expirys Date : 12 Dec 2024
-      </Typography>
-    </Box>
+          <Typography  sx={{
+            fontWeight : 'light'
+            }} variant='small'>
+            Expirys Date
+            <br/>
+            <span style={{
+              color : '#6c757d!important',
+              fontSize : 14
+            }}>
+              12 Dec 2024
+            </span>
+          </Typography>
+        </Box>
+            {/* <Badge badgeContent={"Expired"} color="error" /> */}
+      </Box>
 
-
-  </Grid>
+      </Grid>
+      <Grid xs={12} md={12} sx={{
+        p : 2
+      }}>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Plan Name</TableCell>
+                <TableCell align="center">Months</TableCell>
+                <TableCell align="center">Purcahsed At</TableCell>
+                <TableCell align="center">Expired At</TableCell>
+                <TableCell align="center">Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.planName}
+                  </TableCell>
+                  <TableCell align="center">{row.month}</TableCell>
+                  <TableCell align="center">{row.createdAt}</TableCell>
+                  <TableCell align="center">{row.updatedAt}</TableCell>
+                  <TableCell align="center">
+                    <Badge badgeContent={row.status} color='error' />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   </Grid>
  </>
   )
