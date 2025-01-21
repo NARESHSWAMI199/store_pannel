2e06f50d3ddb2b03f61f16643ebc5f2a8642267d
ashutoshdil <rahul.a@dil.in>
2025-01-21 16:05:03 +0530

    download image

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -235,12 +235,7 @@ function Page() {
     }, [chatMessage]);
 
     const handleDownloadImage = (url) => {
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = url.split('/').pop();
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        window.open(url)
     };
 
     return (
