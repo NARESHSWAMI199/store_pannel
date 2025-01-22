456474d5128bfca96f937896c761739865898d20
ashutoshdil <rahul.a@dil.in>
2025-01-22 12:22:39 +0530

    ui update

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -308,47 +308,54 @@ function Page() {
     };
 
 
-    const showMessage = (message,justifyMessage,time) => (
-    <>
-        <Box sx={{display : 'flex',justifyContent: justifyMessage, width : '100%'}}>
-            <Box sx={{display : 'flex',maxWidth : '40%'}}
-                onMouseEnter={handleMouseEnter} 
-                onMouseLeave={handleMouseLeave}
-            >
-                {/* Message block */}
-                <Box sx={{ px: 1.5, py: 1, boxShadow: 2, background: '#f0f0f5', borderRadius: 2, mx: 1, my: 0.5}}>
-                    <Box sx={{ display: 'flex', flexDirection: message.imagesUrls?.length > 0 ? 'column' : 'row' }}>
-                        {message.imagesUrls && message.imagesUrls.map((url, imgIndex) => (
-                            <Box key={imgIndex} sx={{ position: 'relative', marginBottom: '8px' }}>
-                                <img src={url} alt={`message-img-${imgIndex}`} style={{ width: '100%' }} />
-                                <IconButton sx={{ position: 'absolute', top: 0, right: 0 }} onClick={() => handleDownloadImage(url)}>
-                                    <OpenInNewIcon />
-                                </IconButton>
-                            </Box>
-                        ))}
-                        <Typography sx={{ mx: 1 }}>{message.message}</Typography>
-                        <Typography variant='small' sx={{ fontSize: 10, alignSelf: 'flex-end', mr: 1 }}>{time}</Typography>
-                        {message.sender === user?.slug &&
-                            <DoneAllTwoToneIcon sx={{ fontSize: 14, alignSelf: 'flex-end', color: message.seen ? '#0e6f87' : 'black' }} />
-                        }
+    const showMessage = (message,index) => {
+        let time = format(message.time || 0, "hh:mm");
+        let justifyMessage = message.sender === user?.slug ? 'flex-end' : 'flex-start';
+        return (
+            (message.sender === user?.slug && message.receiver === receiver?.slug) ||
+            (message.sender === receiver?.slug && message.receiver === user?.slug)
+        ) && (
+            <Box key={index} sx={{display : 'flex'}}>
+            <Box sx={{display : 'flex',justifyContent: justifyMessage, width : '100%'}}>
+                <Box sx={{display : 'flex',maxWidth : '40%'}}
+                    onMouseEnter={handleMouseEnter} 
+                    onMouseLeave={handleMouseLeave}
+                >
+                    {/* Message block */}
+                    <Box sx={{ px: 1.5, py: 1, boxShadow: 2, background: '#f0f0f5', borderRadius: 2, mx: 1, my: 0.5}}>
+                        <Box sx={{ display: 'flex', flexDirection: message.imagesUrls?.length > 0 ? 'column' : 'row' }}>
+                            {message.imagesUrls && message.imagesUrls.map((url, imgIndex) => (
+                                <Box key={imgIndex} sx={{ position: 'relative', marginBottom: '8px' }}>
+                                    <img src={url} alt={`message-img-${imgIndex}`} style={{ width: '100%' }} />
+                                    <IconButton sx={{ position: 'absolute', top: 0, right: 0 }} onClick={() => handleDownloadImage(url)}>
+                                        <OpenInNewIcon />
+                                    </IconButton>
+                                </Box>
+                            ))}
+                            <Typography sx={{ mx: 1 }}>{message.message}</Typography>
+                            <Typography variant='small' sx={{ fontSize: 10, alignSelf: 'flex-end', mr: 1 }}>{time}</Typography>
+                            {message.sender === user?.slug &&
+                                <DoneAllTwoToneIcon sx={{ fontSize: 14, alignSelf: 'flex-end', color: message.seen ? '#0e6f87' : 'black' }} />
+                            }
+                        </Box>
                     </Box>
                 </Box>
+            </Box> 
+            {/* Three dots menu */}
+            <IconButton sx={{
+                justifyContent : 'flex-end'
+            }} 
+                onClick={(e) => handleMenuOpen(e, message)}>
+                <MoreVertIcon sx={{ 
+                    opacity: isHovered ? 1 : 0, 
+                    transition: 'opacity 0.3s ease-in-out' ,
+                    fontSize : 18,
+                    color: 'black'
+                    }} />
+            </IconButton>
             </Box>
-        </Box> 
-        {/* Three dots menu */}
-        <IconButton sx={{
-            justifyContent : 'flex-end'
-        }} 
-            onClick={(e) => handleMenuOpen(e, message)}>
-            <MoreVertIcon sx={{ 
-                opacity: isHovered ? 1 : 0, 
-                transition: 'opacity 0.3s ease-in-out' ,
-                fontSize : 18,
-                color: 'black'
-                }} />
-        </IconButton>
-    </>
-    )
+        )
+    }
 
 
     return (
@@ -409,32 +416,14 @@ function Page() {
                                             <Typography sx={{ mx: 2, fontSize: 14 }}>{date}</Typography>
                                             <Box sx={{ flex: 1, height: '1px', backgroundColor: '#ccc' }} />
                                         </Box>
-                                        {pastMessages[date].map((message, index) => {
-                                            let time = format(message.time || 0, "hh:mm");
-                                            let justifyMessage = message.sender === user?.slug ? 'flex-end' : 'flex-start';
-                                            return (
-                                                (message.sender === user?.slug && message.receiver === receiver?.slug) ||
-                                                (message.sender === receiver?.slug && message.receiver === user?.slug)
-                                            ) && (
-                                                <Box key={index} sx={{display : 'flex'}}>
-                                                   {showMessage(message,justifyMessage,time)}
-                                                </Box>
-                                            );
-                                        })}
+                                        {pastMessages[date].map((message, index) => (
+                                          showMessage(message,index) 
+                                        ))}
                                     </React.Fragment>
                                 ))}
-                                {messages.map((message, index) => {
-                                    let time = format(message.time || 0, "hh:mm");
-                                    let justifyMessage = message.sender === user?.slug ? 'flex-end' : 'flex-start';
-                                    return (
-                                        (message.sender === user?.slug && message.receiver === receiver?.slug) ||
-                                        (message.sender === receiver?.slug && message.receiver === user?.slug)
-                                    ) && (
-                                        <Box key={index} sx={{display : 'flex'}}>
-                                            {showMessage(message,justifyMessage,time)}
-                                        </Box>
-                                    );
-                                })}
+                                {messages.map((message, index) => (
+                                    showMessage(message,index) 
+                                ))}
                             </Box>
                             <Box sx={{ position: 'absolute', bottom: 2, minWidth: `calc(100% - ${menuDivWidth}px)`, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                                 <EmojiPicker open={openEmojis} width={'100%'} onEmojiClick={(emojiObj) => setChatMessage(prev => `${prev || ''} ${emojiObj.emoji}`)} />
