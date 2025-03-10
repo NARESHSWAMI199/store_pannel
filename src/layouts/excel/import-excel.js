7f5febe279b881ec9590a1f21f285f38717a2f53
ashutoshdil <rahul.a@dil.in>
2025-03-10 12:32:09 +0530

    import export excel sheets

--- src/layouts/excel/import-excel.js
+++ src/layouts/excel/import-excel.js
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
-import { SvgIcon } from '@mui/material';
+import { InboxOutlined } from '@ant-design/icons';
 import ArrowUpOnSquareIcon from '@heroicons/react/24/solid/ArrowUpOnSquareIcon';
-import { UploadOutlined } from '@ant-design/icons';
-import { Space, Upload } from 'antd';
-import { Form } from 'formik';
-
+import CloseIcon from '@mui/icons-material/Close';
+import { SvgIcon } from '@mui/material';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
+import { Upload, message } from 'antd';
+import axios from 'axios';
+import * as React from 'react';
+import { useState } from 'react';
+import { host } from 'src/utils/util';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -24,47 +24,81 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   },
 }));
 
-export default function DialogFormForExcelImport({importExcelSheet}) {
-  const [open, setOpen] = React.useState(false);
+export default function DialogFormForExcelImport({ importExcelSheet }) {
+  const [open, setOpen] = useState(false);
+  const [fileList, setFileList] = useState([]);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
+
   const handleClose = () => {
     setOpen(false);
+    setFileList([]);
   };
 
-  const importExcelSheetSubmit = (e) => {
-    e.preventDefault();
-    let result =  importExcelSheet(e);
-    if(result) handleClose();
-  }
+  const handleUpload = async () => {
+    if (fileList.length === 0) {
+      message.error('Please select a file to upload.');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('excelfile', fileList[0]);
+    const success = await importExcelSheet(formData);
+    if (success) {
+      handleClose();
+    }
+  };
+
+  const downloadTemplate = () => {
+    axios.get(host+'/admin/item/download/update/template', { responseType: 'blob' })
+      .then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'import_template.xlsx');
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+      })
+      .catch(error => {
+        console.error('Error downloading the template', error);
+        message.error('Error downloading the template');
+      });
+  };
+
+  const props = {
+    onRemove: (file) => {
+      setFileList([]);
+    },
+    beforeUpload: (file) => {
+      setFileList([file]);
+      return false;
+    },
+    fileList,
+  };
 
   return (
     <React.Fragment>
-       <Button
-          color="inherit"
-          onClick={handleClickOpen}
-          startIcon={(
-            <SvgIcon fontSize="small">
-              <ArrowUpOnSquareIcon />
-            </SvgIcon>
-          )}
-        >
-          Import
-        </Button>
-
+      <Button
+        color="inherit"
+        onClick={handleClickOpen}
+        startIcon={(
+          <SvgIcon fontSize="small">
+            <ArrowUpOnSquareIcon />
+          </SvgIcon>
+        )}
+      >
+        Import
+      </Button>
 
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <DialogTitle
-          sx={{ m: 0, p: 2}}
-          id="customized-dialog-title"
-        >
-            Import Items
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Import Items
         </DialogTitle>
         <IconButton
           aria-label="close"
@@ -79,30 +113,22 @@ export default function DialogFormForExcelImport({importExcelSheet}) {
           <CloseIcon />
         </IconButton>
         <DialogContent dividers>
-        <form
-          encType='multipart'
-          onSubmit={ (e) => importExcelSheetSubmit(e)}
-        >
-          <label htmlFor="file" >Import Excel</label>
-          <input
-            type='file'
-            name="excelfile"
-            id="file"
-          ></input>
-          <Button
-            type="primary"
-            htmltype="submit"
-          >
-            UPLOAD EXCEL
-        </Button>
-        </form>
+          <Upload.Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+            <p className="ant-upload-hint">Support for a single upload. Strictly prohibit from uploading company data or other band files</p>
+          </Upload.Dragger>
         </DialogContent>
         <DialogActions>
-          {/* <Button type="submit" autoFocus onClick={handleClose}>
+          <Button onClick={downloadTemplate} color="primary">
+            Download Template
+          </Button>
+          <Button onClick={handleUpload} color="primary">
             Upload Excel
-          </Button> */}
-
-        </DialogActions> 
+          </Button>
+        </DialogActions>
       </BootstrapDialog>
     </React.Fragment>
   );
--- src/sections/items-header.js
+++ src/sections/items-header.js
@@ -1,7 +1,7 @@
 import ArrowDownOnSquareIcon from "@heroicons/react/24/solid/ArrowDownOnSquareIcon"
 import ArrowUpOnSquareIcon from "@heroicons/react/24/solid/ArrowUpOnSquareIcon"
 import PlusIcon from "@heroicons/react/24/solid/PlusIcon"
-import { Box, Button, Container, Stack, SvgIcon, Typography } from "@mui/material"
+import { Alert, Box, Button, Container, Snackbar, Stack, SvgIcon, Typography } from "@mui/material"
 import axios from "axios"
 import Head from "next/head"
 import Link from "next/link"
@@ -21,12 +21,16 @@ export function ItemHeaders(props){
     const [message, setMessage] = useState("")
     const [flag, setFlag] = useState("warning")
 
+    // Open only in error case
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [downloadUrl, setDownloadUrl] = useState('');
+    // Import excel sheet
     const importItemExcelSheet = async (formData) => {
         let success = false;
         axios.defaults.headers = {
           Authorization: auth.token
         };
-        await axios.post(host + '/admin/item/importExcel', formData,{
+        await axios.post(host + '/wholesale/item/importExcel', formData,{
           headers: {
             'Content-Type': 'multipart/form-data'
           }
@@ -36,7 +40,7 @@ export function ItemHeaders(props){
             setFlag("success");
             if(res.status === 201) {
                 let fileUrl = resData.fileUrl;
-                alert(fileUrl);
+                // if fileUrl is not empty then download the file
                 axios.get(fileUrl, { responseType: 'blob' })
                 .then(response => {
                     const url = window.URL.createObjectURL(new Blob([response.data], 
@@ -65,6 +69,8 @@ export function ItemHeaders(props){
       }
 
 
+     // Export excel sheet
+
       const exportExcelSheet = async () => {
         const confirmDownload = window.confirm("Are you sure you want to download the Excel sheet?");
         if (!confirmDownload) {
@@ -74,6 +80,7 @@ export function ItemHeaders(props){
         axios.defaults.headers = {
             Authorization: auth.token
         }
+        
         await axios.post(host + '/wholesale/item/exportExcel', !!props.searchFilters ? {...props.searchFilters} : undefined, { responseType: 'blob' })
             .then(response => {
                 const url = window.URL.createObjectURL(new Blob([response.data], 
@@ -96,8 +103,56 @@ export function ItemHeaders(props){
             })
     }
 
+
+
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.setAttribute('download', projectName + '_itemNotUpdated_items.xlsx');
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+        setFlag("success")
+        setMessage("Successfully dowloaded not updated items sheet.")
+        setOpen(true)
+        setSnackbarOpen(false);
+    };
+
+
+       /** for snackbar close */
+    const handleClose = () => {
+        setOpen(false)
+    };
+
+    
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false);
+    };
     return (
         <>
+            <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={open}
+                onClose={handleClose}
+                key={'top' + 'right'}
+            >
+                <Alert onClose={handleClose} severity={flag} sx={{ width: '100%' }}>
+                    {message}
+                </Alert>
+            </Snackbar>
+
+            {/* this is a diffrent kind of snackbar using only for a specific case | when some item not update during import excel sheet */}
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={6000}
+                onClose={handleSnackbarClose}
+                message="Some items were not updated. Click to view."
+                action={
+                    <Button color="secondary" size="small" onClick={handleDownload}>
+                        VIEW
+                    </Button>
+                }
+            />
+
             <Stack spacing={1}> 
                 <Typography variant="h4"> 
                     {props.headerTitle}
